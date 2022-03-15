3d02157f2cc4fe512f79c724cf8ed78e399c364d
Yasir Choudhury <yasir2493@gmail.com>
2022-03-15 13:59:39 -0400

    Added conditional component rendering, componenent styling and sidebar component that users can select pages from

--- src/pages/Panel/App.tsx
+++ src/pages/Panel/App.tsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
+import Header from "./Components/AppBar";
+import Playground from '/Users/yasir/Desktop/obsidian-developer-tool/src/pages/Panel/Components/Routes/Playground.js'
+import Cache from '/Users/yasir/Desktop/obsidian-developer-tool/src/pages/Panel/Components/Routes/Cache.js'
+import Performance from  '/Users/yasir/Desktop/obsidian-developer-tool/src/pages/Panel/Components/Routes/Performance.js'
+
 
 
 
 const App = () => {
-  
+  const [count, setCount] = useState<number>(0);
 
+
+  
   return (
-    <div>hello world!</div>
+    <> 
+    <div>
+     <Header count={count} setCount={setCount} />
+    </div>
+
+     <div> 
+     {(count===1) ? (
+                <Performance/>
+              ) : (
+             <div> </div>
+              )}
+      {(count===2) ? (
+                <Cache/>
+              ) : (
+             <div> </div>
+              )}
+       {(count===3) ? (
+                <Playground/>
+              ) : (
+             <div> </div>
+              )}
+     </div>
+
+     </>
   );
 };
 
