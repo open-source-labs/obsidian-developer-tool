1cf8b9871e4752b6cfaab3b1a564c18424a57168
Yasir Choudhury <yasir2493@gmail.com>
2022-03-16 10:07:35 -0400

    updated filepaths and updated JS integration for cache display

--- src/pages/Panel/Components/Routes/Cache.js
+++ src/pages/Panel/Components/Routes/Cache.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
-// import  javascript  from '@codemirror/lang-javascript'
+import {javascript } from '@codemirror/lang-javascript'
 import { oneDark } from '@codemirror/theme-one-dark';
 
 
@@ -15,7 +15,7 @@ const Cache = () => {
        left='225px'
        style={{marginLeft: 225}}
        marginTop
-      //  extensions={[javascript({ jsx: true })]}
+       extensions={[javascript({ jsx: true })]}
       theme={oneDark}
        onChange={(value, viewUpdate) => {
          console.log('value:', value);
