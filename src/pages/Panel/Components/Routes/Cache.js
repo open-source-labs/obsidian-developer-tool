6ab00551185ff80384afaa6f1c5ba8c5737eceb7
Yasir Choudhury <yasir2493@gmail.com>
2022-03-15 20:48:44 -0400

    Added graphQL cache display component

--- src/pages/Panel/Components/Routes/Cache.js
+++ src/pages/Panel/Components/Routes/Cache.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+// import  javascript  from '@codemirror/lang-javascript'
+import { oneDark } from '@codemirror/theme-one-dark';
 
 
 const Cache = () => {
 
-  
-  
-    
-    return (
-   <div>The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.</div>
-    );
+  return (
+     <CodeMirror
+       value="Cache:"
+       height="400px"
+       width='100%'
+       position='absolute'
+       left='225px'
+       style={{marginLeft: 225}}
+       marginTop
+      //  extensions={[javascript({ jsx: true })]}
+      theme={oneDark}
+       onChange={(value, viewUpdate) => {
+         console.log('value:', value);
+       }}
+    />
+
+   );
   };
   
   export default Cache;
