6056f8acbafe0865b7f9048a472dce97d659c59a
Yasir Choudhury <yasir2493@gmail.com>
2022-03-17 14:15:20 -0400

    implemented scss functionality for sidebar

--- src/pages/Panel/Components/AppBar.tsx
+++ src/pages/Panel/Components/AppBar.tsx
@@ -11,13 +11,13 @@ import {
 
 import { FiHome, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
 import { BiCog } from "react-icons/bi";
-import {MdExplore} from "react-icons/Md"
+import {MdExplore, MdTrendingUp} from "react-icons/Md"
 import {GoDatabase} from 'react-icons/Go'
 import {GrDocumentPerformance} from 'react-icons/Gr'
 
 
 import "react-pro-sidebar/dist/css/styles.css";
-import "./AppBar.css";
+import "./AppBar.scss";
 
 
 type Props = {
@@ -36,7 +36,7 @@ const Header = (props:Props) => {
 
   return (
     <>
-      <div id="header">
+       <div id="header">
         <ProSidebar collapsed={menuCollapse}>
             
           <SidebarHeader>
@@ -57,17 +57,14 @@ const Header = (props:Props) => {
           </SidebarHeader>
           <SidebarContent>
             <Menu iconShape="square">
-              <MenuItem active={false} icon={<FiHome />}>
-                Home
-              </MenuItem>
-              <MenuItem onClick={() => props.setCount(props.count=1)} icon={<GrDocumentPerformance />}>Performance</MenuItem>
+              <MenuItem onClick={() => props.setCount(props.count=1)} icon={<MdTrendingUp />}>Performance</MenuItem>
               <MenuItem onClick={() => props.setCount(props.count=2)} icon={<GoDatabase />}>Cache</MenuItem>
               <MenuItem onClick={() => props.setCount(props.count=3)} icon={<MdExplore />}>Playground</MenuItem>
               <MenuItem onClick={() => props.setCount(props.count=4)} icon={<BiCog />}>Settings</MenuItem>
             </Menu>
           </SidebarContent>
         </ProSidebar>
-      </div>
+        </div>
     </>
   );
 };
