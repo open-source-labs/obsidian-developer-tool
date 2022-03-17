15daa8be03ab8bb2d5411c9248a07b42de9145bd
Anthony Guan <guananthony@gmail.com>
2022-03-16 17:48:31 -0700

    CLCache Co-authored-by: Yasir Choudhury <Yasir-Choudhury@users.noreply.github.com> Co-authored-by: YuriiShchyrba <YuriiShchyrba@users.noreply.github.com>

---
+++ src/main.js
@@ -0,0 +1,9 @@
+window.alert('Content script loaded');
+
+const clearCacheButton = document.getElementById('clearCacheButton');
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if ((request.clearCache = true)) {
+		clearCacheButton.click();
+	}
+});
