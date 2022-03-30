88fb9d208518c448fd2c78cb1686ca438d6677ca
YuriiShchyrba <yurashchyrba@gmail.com>
2022-03-30 10:03:13 -0400

    feat: changes

--- src/pages/Panel/Components/QueryOutput.tsx
+++ src/pages/Panel/Components/QueryOutput.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Controlled as CodeMirror } from 'react-codemirror2';
-import 'codemirror';
-import 'codemirror-graphql/lint';
-import 'codemirror-graphql/hint';
-import 'codemirror-graphql/mode';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material-darker.css';
-import 'codemirror/theme/xq-light.css';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { oneDark } from '@codemirror/theme-one-dark';
+// import { Controlled as CodeMirror } from 'react-codemirror2';
+// import 'codemirror';
+// import 'codemirror-graphql/lint';
+// import 'codemirror-graphql/hint';
+// import 'codemirror-graphql/mode';
+// import 'codemirror/lib/codemirror.css';
+// import 'codemirror/theme/material-darker.css';
+// import 'codemirror/theme/xq-light.css';
 import './playground.css';
 
 //component for graphQL query output data within playground
@@ -47,16 +50,29 @@ const QueryOutput = (props) => {
     <div className='queryOutput'>
       <div className="head-text"># GraphQL Query Results</div>
       <CodeMirror
-        value={output}
+				value={output} 
+				// height='100%'
+				// width='100%'
+				//  position='absolute'
+				//  left='225px'
+				// marginTop
+				extensions={[javascript({ jsx: true })]}
+				theme={oneDark}
+        // onChange={(editor, data, value) => {
+        //   setOutput(props.data.toString());
+        // }}
+			/>
+      {/* <CodeMirror
+        value={output} 
         options={{
           theme: 'material-darker',
           lineNumbers: false,
-          mode: 'json',
+          mode: 'graphql',
         }}
         onBeforeChange={(editor, data, value) => {
           setOutput(props.data.toString());
         }}
-      />
+      /> */}
     </div>
   );
 };
